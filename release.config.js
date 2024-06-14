c45df3d490131392819623e8c860f204e0cd2be3
gnmeyer <grant.meyer@liatrio.com>
2024-06-14 15:41:10 -0700

    ci: add fetch depth to action #19

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 const config={
-    branches: ['main'], //only release on main branch
-    dryRun: true, //set to false to actually release
+    branches: ['fetch-depth'], //only release on main branch
+    dryRun: false, //set to false to actually release
     debug: false, //set to false to turn off debugging
     plugins: [
         '@semantic-release/commit-analyzer', //added by default
