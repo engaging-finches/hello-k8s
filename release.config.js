e65903d483b246a6003f9d82da6a5e2d1f670ee6
gnmeyer <grant.meyer@liatrio.com>
2024-06-14 14:52:19 -0700

    ci: Adds semantic-release #19

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config={
-    branches: ['main', 'semantic-releases'], //only release on main branch
+    branches: ['main'], //only release on main branch
     dryRun: true, //set to false to actually release
     debug: true, //set to false to turn off debugging
     plugins: [
@@ -9,11 +9,6 @@ const config={
         },
         '@semantic-release/release-notes-generator', //added by default
         '@semantic-release/github', //added by default
-        "@semantic-release/changelog",
-        ["@semantic-release/git", {
-            "assets": ["ghrunner/dist/*.yaml", "CHANGELOG.md"], //commits these files back into the repo
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}" //skip commits with [skip ci]
-        }],
     ]
 
 }
