9f313ef2763b86f82578b7c21fe80a906980485d
gnmeyer <grant.meyer@liatrio.com>
2024-06-14 13:42:17 -0700

    ci: Adds semantic-release #19

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ const config={
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         ["@semantic-release/git", {
-            "assets": ["dist/*.js", "dist/*js.map"],
+            "assets": ["/ghrunner", "/user_guides", "/docker", "package-lock.json", "package.json", "README.md",],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
