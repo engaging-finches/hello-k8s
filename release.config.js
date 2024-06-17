8f1126c8c57ecd1b15c53c4088b3b2d65b789d4b
gnmeyer <grant.meyer@liatrio.com>
2024-06-17 12:29:31 -0700

    chore: edit semantic release workflow to only package the dist/install.yaml #78

--- release.config.js
+++ release.config.js
@@ -1,17 +1,18 @@
-const config={
-    branches: ['main'], //only release on main branch
-    dryRun: false, //set to false to actually release
-    debug: false, //set to false to turn off debugging
-    plugins: [
-        '@semantic-release/commit-analyzer', //added by default
-        {
-            "preset": "conventionalcommits"
-        },
-        '@semantic-release/release-notes-generator', //added by default
-        '@semantic-release/github', //added by default
-    ],
-    repositoryUrl: "https://github.com/engaging-finches/hello-k8s",
-    initialVersion: "0.1.0"
+const config = {
+  branches: ['main'], //only release on main branch
+  dryRun: false, //set to false to actually release
+  debug: false, //set to false to turn off debugging
+  plugins: [
+    '@semantic-release/commit-analyzer', //added by default
+    {
+      "preset": "conventionalcommits"
+    },
+    '@semantic-release/release-notes-generator', //added by default
+    ['@semantic-release/github', {
+      assets: ['ghrunner/dist/install.yaml'], // Include only 'dist/install.yaml' in the release
+    }],
+  ],
+  repositoryUrl: "https://github.com/engaging-finches/hello-k8s",
 }
 
-module.exports=config;
+module.exports = config;
